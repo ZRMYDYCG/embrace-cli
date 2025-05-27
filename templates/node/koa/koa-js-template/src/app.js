5a5678a2607880d5cee0d917a70cb9b921c0aa12
ZRMYDYCG <547471919@qq.com>
2025-05-27 22:12:54 +0800

    feat ✨: A new routing architecture is added

--- templates/node/koa/koa-js-template/src/app.js
+++ templates/node/koa/koa-js-template/src/app.js
@@ -2,6 +2,7 @@ import Koa from 'koa'
 import { koaCore } from './core/koa-core.js'
 import { loadMiddlewares } from './core/loader.js'
 import { loadConfig } from '../config/index.js'
+import { moduleRouters } from './modules/index.js'
 
 const app = new Koa()
 
@@ -13,6 +14,12 @@ koaCore(app)
 
 // 自动加载中间件
 loadMiddlewares(app)
+
+// 使用模块路由
+moduleRouters.forEach(moduleRouter => {
+  app.use(moduleRouter.routes());
+  app.use(moduleRouter.allowedMethods());
+})
 
 // 启动服务器
 app.listen(config.server.port, () => {
